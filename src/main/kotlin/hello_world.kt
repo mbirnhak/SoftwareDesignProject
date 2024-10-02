76a8b278012c035e6f17a4d8f058baf34337baa3
mbirnhak <mrbirnhak@aol.com>
2024-10-02 10:28:39 -0400

    Hello World file updates

--- src/main/kotlin/hello_world.kt
+++ src/main/kotlin/hello_world.kt
@@ -1,3 +1,6 @@
+import java.time.LocalDate
+import java.time.Month
+
 fun start(): String = "OK"
 
 fun main() {
@@ -5,4 +8,17 @@ fun main() {
 
     val x = "Hello, world!"
     println("The string is ${x.length}")
+
+    val p = Person("Matt", LocalDate.of(2003, Month.JULY, 17))
+    println(p)
+}
+
+class Person(val name: String, val dob: LocalDate) {
+    fun age(): Int {
+        return LocalDate.now().year - dob.year
+    }
+
+    override fun toString(): String {
+        return "$name is ${age()} years old"
+    }
 }
