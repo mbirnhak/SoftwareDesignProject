741248de9287a39b96d568a19c852dc422a89d52
mbirnhak <mrbirnhak@aol.com>
2024-11-09 13:25:35 -0500

    Ollama POST request HW assignment.

--- src/main/kotlin/edu/trincoll/ollama/ollama.kt
+++ src/main/kotlin/edu/trincoll/ollama/ollama.kt
@@ -6,6 +6,9 @@ import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.*
 import io.ktor.http.*
 import kotlinx.serialization.Serializable
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
 
 @Serializable
 data class OllamaRequest(
@@ -17,17 +20,28 @@ data class OllamaRequest(
 @Serializable
 data class OllamaResponse(
     val model: String,
-    val responses: String
+    val response: String
 )
 
 suspend fun main() {
-    val client = HttpClient(CIO)
+    val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
+        }
+        engine {
+            requestTimeout = 60_000 // Was giving a timeout error without this
+        }
+    }
 
     val response = client.post("http://localhost:11434/api/generate") {
         contentType(ContentType.Application.Json)
-        setBody(OllamaRequest("llama3.2", "Why is the sky blue?", false))
+        accept(ContentType.Application.Json)
+        setBody(OllamaRequest("llama3.2", "Explain networking protocols to me", false))
     }
 
+    val jsonParser = Json{ ignoreUnknownKeys = true } //ignore Json keys that are not response or model
+    val ollamaResponse = jsonParser.decodeFromString<OllamaResponse>(response.body<String>())
+
     println(response.status.value)
-    println(response.body<String>())
+    println(ollamaResponse.response)
 }
